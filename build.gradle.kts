5bd8b94aaf7c2175a803c34b60cfde6d2f6f5c7c
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2026-03-07 23:24:07 -0500

    chore: regenerate project files
    Semver Impact: NONE

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 repositories {
     mavenCentral()
-
-    maven("https://jitpack.io")
 }
 
 group   = "one.wabbit"
 version = "0.1.0"
 
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("org.jetbrains.dokka") version "2.0.0"
-    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+    kotlin("jvm")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
 
-    kotlin("plugin.serialization") version "2.2.20"
+    kotlin("plugin.serialization")
 
     id("maven-publish")
 }
@@ -32,7 +30,7 @@ publishing {
 }
 
 dependencies {
-    implementation("one.wabbit:kotlin-data-ref:1.1.1")
+    implementation(project(":kotlin-data-ref")) // 1.1.1
 
     testImplementation(kotlin("test"))
 
@@ -61,7 +59,9 @@ tasks {
     withType<KotlinCompile> {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_21)
-            freeCompilerArgs.add("-Xcontext-receivers")
+
+            freeCompilerArgs.add("-Xcontext-parameters")
+
         }
     }
 
